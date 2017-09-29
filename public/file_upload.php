729ac6e98bb1f988011aed2ef24a4788b004a446
Michael Cabelin <ncabelin@gmail.com>
2017-09-29 00:18:21 -0700

    file upload test

---
+++ public/file_upload.php
@@ -0,0 +1,15 @@
+<?php
+phpinfo();
+// echo 'Hello';
+// $target_dir = "uploads/";
+// $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+// $uploadOk = 1;
+// $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// // Check if image file is a actual image or fake image
+// if($_SERVER['REQUEST_METHOD'] == 'POST') {
+// 		$file = $_FILES['fileToUpload'];
+//     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+//     	echo 'Uploaded';
+//     }
+// }
+?>
